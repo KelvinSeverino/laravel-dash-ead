a40ab6c829ec8e819311670abb68d156a96a1214
Kelvin Severino <kseveri@outlook.com>
2023-05-28 08:46:48 -0300

    Service e Repository c/ Inversão de Dependencia

--- source/laravel/app/Providers/AppServiceProvider.php
+++ source/laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\{
+    UserRepository
+};
+use App\Repositories\{
+    UserRepositoryInterface
+};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        //Bind das interfaces, se trocar o ORM para outro, mudar aqui, usando conceito da Inversao de Dependencia
+        //singleton gera o New Class() uma vez e armazena para ser reutilizado posteriormente
+        $this->app->singleton(
+            UserRepositoryInterface::class,
+            UserRepository::class,
+        );
     }
 
     /**
