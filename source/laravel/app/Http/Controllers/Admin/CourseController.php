42dcaf0d14cc3793521c8f96a227f03a380ae44d
Kelvin Severino <kseveri@outlook.com>
2023-06-03 13:40:34 -0300

    Form dos Cursos

--- source/laravel/app/Http/Controllers/Admin/CourseController.php
+++ source/laravel/app/Http/Controllers/Admin/CourseController.php
@@ -26,7 +26,7 @@ class CourseController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.courses.create');
     }
 
     public function store(Request $request)
