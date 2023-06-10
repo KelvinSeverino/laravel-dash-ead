34d31ba846759dac703750fc7a29d7927e7bfa73
Kelvin Severino <kseveri@outlook.com>
2023-06-10 20:24:51 -0300

    Listagem de duvidas

--- source/laravel/app/Models/Support.php
+++ source/laravel/app/Models/Support.php
@@ -12,4 +12,20 @@ class Support extends Model
     protected $fillable = [
         'status', 'description', 'user_id', 'lesson_id'
     ];
+
+    /**
+     * user - gera relacionamento entre tabelas / traz o aluno do suporte
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * lesson - gera relacionamento entre tabelas / traz o a aula do suporte
+     */
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
 }
